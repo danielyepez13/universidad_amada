6b8a82c03c544bda7ed4c3580ebedf4a5aa3c7ff
danielyepez13 <danielyepez62@gmail.com>
2023-05-31 20:28:11 -0400

    Testimonio parte 1

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <title>Inicio</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.12.1/dist/cdn.min.js"></script>
+    <script src="./js/main.js"></script>
 </head>
 
 <body>
@@ -62,57 +63,107 @@
             </div>
         </div>
     </nav>
-    <div class="mt-16 mx-4 flex flex-wrap flex-col md:flex-row justify-center">
-        <div class="flex flex-col max-w-lg">
-            <h1 class="text-4xl font-bold text-gray-800 mb-5">La universidad de tus sueños está justo aquí</h1>
-            <p class="mb-5 text-gray-600">Nosotros trabajamos para dar la máxima garantía de los mejores estudios dentro de nuestras instalaciones.</p>
-            <div class="flex flex-row justify-around">
-                <div class="flex flex-col space-y-4">
-                    <div class="p-4 flex flex-wrap items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-                        </svg> 
-                        <p class="ml-2">Profesores Graduados</p>
+    <div class="md:mt-12 mt-8 mx-4 flex flex-wrap flex-col md:flex-row justify-center">
+        <div class="flex flex-col justify-around max-w-xl">
+            <h1 class="text-4xl font-bold text-gray-800 mb-3 text-center">La universidad de tus sueños está justo aquí</h1>
+            <div class="hidden md:block">
+                <p class="text-gray-600 text-xl">Nosotros trabajamos para dar la máxima garantía de los mejores estudios dentro de nuestras instalaciones.</p>
+                <div class="flex flex-row justify-around">
+                    <div class="flex flex-col space-y-4">
+                        <div class="p-4 flex flex-wrap items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+                            </svg>
+                            <p class="ml-2">Profesores Graduados</p>
+                        </div>
+                        <div class="p-4 flex flex-wrap items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+                            </svg>
+                            <p class="ml-2">Horarios a disposición</p>
+                        </div>
+                        <div class="p-4 flex flex-wrap items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+                            </svg>
+                            <p class="ml-2">Instalaciones recreativas</p>
+                        </div>
                     </div>
-                    <div class="p-4 flex flex-wrap items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-                        </svg> 
-                        <p class="ml-2">Horarios a disposición</p>
-                    </div>
-                    <div class="p-4 flex flex-wrap items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-                        </svg> 
-                        <p class="ml-2">Instalaciones recreativas</p>
-                    </div>
-                </div>
-                <div class="flex flex-col space-y-4">
-                <div class="p-4 flex flex-wrap items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-                        </svg> 
-                        <p class="ml-2">Carreras de gran Preferencia</p>
-                    </div>
-                    <div class="p-4 flex flex-wrap items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-                        </svg> 
-                        <p class="ml-2">Postgrados completos</p>
-                    </div>
-                    <div class="p-4 flex flex-wrap items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-                        </svg> 
-                        <p class="ml-2">Actividades extracurriculares</p>
+                    <div class="flex flex-col space-y-4">
+                        <div class="p-4 flex flex-wrap items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+                            </svg>
+                            <p class="ml-2">Carreras de gran preferencia</p>
+                        </div>
+                        <div class="p-4 flex flex-wrap items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+                            </svg>
+                            <p class="ml-2">Postgrados completos</p>
+                        </div>
+                        <div class="p-4 flex flex-wrap items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+                            </svg>
+                            <p class="ml-2">Actividades extracurriculares</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="max-w-lg overflow-hidden ml-3">
+        <div class="max-w-xl md:ml-3">
             <img class="w-full object-cover rounded-md" src="img/universidad.jpg" alt="">
         </div>
     </div>
+    <section class="bg-white dark:bg-gray-800 mt-8">
+        <div class="max-w-6xl px-6 py-10 mx-auto">
+            <p class="text-xl font-medium text-blue-500 ">Testimonios</p>
+
+            <h1 class="mt-2 text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">
+                ¿Qué dicen nuestros estudiantes?
+            </h1>
+
+            <main class="relative z-20 w-full mt-8 md:flex md:items-center xl:mt-12">
+                <div class="absolute w-full bg-blue-800 -z-10 md:h-96 rounded-2xl"></div>
+
+                <div class="w-full p-6 bg-blue-600 md:flex md:items-center rounded-2xl md:bg-transparent md:p-0 lg:px-12 md:justify-evenly" x-data="getCarouselData()">
+                    <template x-for="image in images.slice(currentIndex, currentIndex + 1)" :key="images.indexOf(image)">
+                        <img class="h-24 w-24 md:mx-6 rounded-full object-cover shadow-md md:h-[32rem] md:w-80 lg:h-[36rem] lg:w-[26rem] md:rounded-2xl" :src="image.imagen" alt="foto de estudiante">
+                    </template>
+                    <!-- <img class="h-24 w-24 md:mx-6 rounded-full object-cover shadow-md md:h-[32rem] md:w-80 lg:h-[36rem] lg:w-[26rem] md:rounded-2xl" src="https://images.unsplash.com/photo-1488508872907-592763824245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="student photo" /> -->
+
+                    <div class="mt-2 md:mx-6">
+                        <template x-for="image in images.slice(currentIndex, currentIndex + 1)" :key="images.indexOf(image)">
+                            <div>
+                                <p class="text-xl font-medium tracking-tight text-white" x-text="image.nombre"></p>
+                                <p class="text-blue-200" x-text="image.carrera"></p>
+                                <p class="mt-4 text-lg leading-relaxed text-white md:text-xl" x-text="image.experiencia"></p>
+                            </div>
+                        </template>
+
+                        <div class="flex items-center justify-between mt-6 md:justify-start">
+                            <button title="Izquierda" class="p-2 text-white transition-colors duration-300 border rounded-full rtl:-scale-x-100 hover:bg-blue-400" @click="decrement()">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
+                                </svg>
+                            </button>
+
+                            <button title="Derecha" class="p-2 text-white transition-colors duration-300 border rounded-full rtl:-scale-x-100 md:mx-6 hover:bg-blue-400" @click="increment()">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    </section>
+
+    <script>
+
+    </script>
 </body>
 
 </html>
---
+++ js/main.js
@@ -0,0 +1,22 @@
+function getCarouselData() {
+    return {
+        currentIndex: 0,
+        images: [
+            {
+                id: 1,  imagen: 'https://images.unsplash.com/photo-1618355776464-8666794d2520?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80', nombre: 'Ema Watson', carrera: 'Estudiante de Administración', experiencia: '“Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quibusdam ducimus libero a”'
+            },
+            {
+                id: 2,  imagen: 'https://images.unsplash.com/photo-1567168539593-59673ababaae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHN0dWRlbnR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60', nombre: 'Steven McFly', carrera: 'Estudiante de Ingenieria Informática', experiencia: '“Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quibusdam ducimus libero a”'
+            },
+            {
+                id: 3,  imagen: 'https://images.unsplash.com/photo-1517256673644-36ad11246d21?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fHN0dWRlbnR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60', nombre: 'Lorena Madison', carrera: 'Estudiante de Ingeniería Agropecuaria', experiencia: '“Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quibusdam ducimus libero a”'
+            }
+        ],
+        increment() {
+            this.currentIndex = this.currentIndex === this.images.length - 1 ? 0 : this.currentIndex + 1;
+        },
+        decrement() {
+            this.currentIndex = this.currentIndex === this.images.length - 1 ? 0 : this.currentIndex - 1;
+        },
+    }
+}
